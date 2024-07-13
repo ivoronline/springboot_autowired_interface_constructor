136fea08bd461f217e619f473fb3d4ce0527357b
ivoronline <ivoronline@gmail.com>
2024-07-13 09:49:27 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_autowired_interface_constructor/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_autowired_interface_constructor/controllers/MyController.java
@@ -1,7 +1,6 @@
 package com.ivoronline.springboot_autowired_interface_constructor.controllers;
 
 import com.ivoronline.springboot_autowired_interface_constructor.services.MyServiceInterface;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
